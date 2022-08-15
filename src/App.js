2eec5f25ede628804385f372ff255bc732e53d00
ogar-simonn <ogarsimon0000@gmail.com>
2022-08-15 12:26:29 +0100

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React from "react";
-import "./index.css";
-import {useGlobalContext} from "./context" 
-import SetupForm from "./setupForm"
-import Loading from "./loading";
-
+import React from 'react';
+import './index.css';
+import { useGlobalContext } from './context';
+import SetupForm from './setupForm';
+import Loading from './loading';
+import Modal from './Modal';
 
 export default function App() {
-  const {waiting, loading, questions, index, error } = useGlobalContext();
-  if(waiting) {
-    return <SetupForm/>
-  }
-  if(loading) {
-    return <Loading/>
+  const { waiting, loading, questions, index, error, correct, nextQuestion, checkAnswer  } =
+    useGlobalContext();
+  if (waiting) {
+    return <SetupForm />;
   }
 
+  if (loading) {
+    return <Loading />;
+  }
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+
   return (
-    <div>
-      <h3>Quiz app</h3>
-    </div>
+    <main>
+      <section className="quiz">
+        <Modal />
+        <p className="correct-answers">
+          {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <h2
+                  key={index}
+                  className="answer-btn"
+                  onClick={() => checkAnswer(correct_answer === answer)}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question" onClick={nextQuestion}>Next question </button>
+      </section>
+    </main>
   );
 }
--- src/context.js
+++ src/context.js
@@ -1,61 +1,99 @@
-import axios from 'axios'
-import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios';
+import React, { useState, useContext, useEffect } from 'react';
 
 const table = {
   sports: 21,
   history: 23,
-  politics: 24,}
-const API_ENDPOINT = 'https://opentdb.com/api.php?'
+  politics: 24,
+};
+const API_ENDPOINT = 'https://opentdb.com/api.php?';
 
 const url = '';
-let tempUrl = "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium";
+let tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=medium';
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [waiting, setWaiting] = useState(true);
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [error, setError] = useState(false)
-  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [error, setError] = useState(false);
+
   const fetchQuestions = async (url) => {
     setLoading(true);
     setWaiting(false);
-    const response = await axios(url).catch(err => console.log(err))
-    if(response) {
+    const response = await axios(url).catch((err) => console.log(err));
+    if (response) {
       const data = response.data.results;
-     if(data.length > 0) {
-       setLoading(false);
-       setQuestions(data);
-       setError(false);
-     } else {
-       setError(true);
-     }
-      
+      if (data.length > 0) {
+        setLoading(false);
+        setQuestions(data);
+        setError(false);
+      } else {
+        setError(true);
+      }
     } else {
-      setWaiting(true)
+      setWaiting(true);
     }
-  }
+  };
+  const nextQuestion = () => {
+    
+    setIndex((prevIndex) => {
+      const index = prevIndex + 1;
+      if (index >= questions.length - 1) {
+        openModal()
+        return 0;
+      } else {
+        return index;
+      }
+    });
+  };
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldVal) => oldVal + 1);
+    }
+    nextQuestion();
+  };
 
-useEffect(() => {
-  fetchQuestions(tempUrl);
-}, [])
-  return <AppContext.Provider value={{
-    waiting,
-    loading,
-    questions, 
-    index,
-    setQuestions,
-    correct,
-    setCorrect,
-  }}>{children}</AppContext.Provider>
-}
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setWaiting(true);
+    setCorrect(0);
+    setIsModalOpen(false);
+  };
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        setQuestions,
+        correct,
+        setCorrect,
+        nextQuestion,
+        checkAnswer,
+        openModal,
+        closeModal,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sue
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
